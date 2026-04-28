66d2c821e6d5d9c8f39dd9a3cd5f5dded077b57e
Javier <javiermacasaet@gmail.com>
2026-04-28 12:51:44 +0800

    Add event fields (capacity, image) and EventSignup model.

--- localevents/models.py
+++ localevents/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import Profile
 
 
 class EventType(models.Model):
@@ -13,18 +14,23 @@ class EventType(models.Model):
 
 
 class Event(models.Model):
-    title = models.CharField(
-        max_length=255
-    )
-    category = models.ForeignKey(
-        EventType,
-        null=True,
-        on_delete=models.SET_NULL
-    )
+    STATUS_CHOICES = [
+        ('Available', 'Available'),
+        ('Full', 'Full'),
+        ('Done', 'Done'),
+        ('Cancelled', 'Cancelled'),
+    ]
+
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(EventType, null=True, on_delete=models.SET_NULL)
+    organizer = models.ManyToManyField(Profile)
+    event_image = models.ImageField(upload_to='images/', null=True, blank=True)
     description = models.TextField()
     location = models.CharField(max_length=255)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    event_capacity = models.PositiveIntegerField()
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES, default='Available')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -33,3 +39,20 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class EventSignup(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='signups')
+    user_registrant = models.ForeignKey(
+        Profile,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='event_signups'
+    )
+    new_registrant = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        if self.user_registrant:
+            return self.user_registrant.display_name
+        return self.new_registrant
