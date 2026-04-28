aa9c0a7dd1572830a5d021f84d509e68cc80d8aa
Javier <javiermacasaet@gmail.com>
2026-04-28 12:52:25 +0800

    Implement event creation, update, and signup views with forms

---
+++ localevents/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Event, EventSignup
+
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = [
+            'title',
+            'category',
+            'event_image',
+            'description',
+            'location',
+            'start_time',
+            'end_time',
+            'event_capacity',
+            'status',
+        ]
+        widgets = {
+            'start_time': forms.DateTimeInput(
+                attrs={'type': 'datetime-local'}
+            ),
+            'end_time': forms.DateTimeInput(
+                attrs={'type': 'datetime-local'}
+            ),
+        }
+
+
+class EventSignupForm(forms.ModelForm):
+    class Meta:
+        model = EventSignup
+        fields = [
+            'new_registrant',
+        ]
