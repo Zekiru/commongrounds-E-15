d0dd4349d6fca6a2e3f272961738e82984545ad0
clpanini <chloelainedpangilinan@gmail.com>
2026-05-02 19:57:46 +0800

    Updated models.py

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from accounts.models import Profile
 
 class BookCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -10,8 +10,8 @@ class BookCategory(models.Model):
         verbose_name = 'book category'
         verbose_name_plural = 'book categories'
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -22,8 +22,16 @@ class Book(models.Model):
         null=True,
         blank=True
     )
+    contributor = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
     author = models.CharField(max_length=255)
+    synopsis = models.TextField(blank=True)
     publication_year = models.IntegerField()
+    available_to_borrow = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -34,3 +42,35 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class BookReview(models.Model):
+    user_reviewer = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    anon_reviewer = models.TextField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    comment = models.TextField()
+
+
+class Bookmark(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    date_bookmarked = models.DateField(auto_now_add=True)
+
+
+class Borrow(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrower = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    name = models.CharField(max_length=255, blank=True)
+    date_borrowed = models.DateField()
+    date_to_return = models.DateField(null=True, blank=True)
