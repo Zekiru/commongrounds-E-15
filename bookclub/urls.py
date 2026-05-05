8427e6fe306a121ad4e28ca7d2f9faaa30340c72
clpanini <chloelainedpangilinan@gmail.com>
2026-05-05 09:57:56 +0800

    Fix PEP8 style of code

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -4,9 +4,24 @@ from . import views
 app_name = "bookclub"
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name="book_list"),
-    path('book/<int:pk>/', views.BookDetailView.as_view(), name="book_detail"),
-    path('book/add/', views.BookCreateView.as_view(), name="book_add"),
-    path('book/<int:pk>/edit/', views.BookUpdateView.as_view(), name="book_edit"),
-    path('book/<int:pk>/borrow/', views.BookBorrowView.as_view(), name="book_borrow"),
+    path(
+        'books/',
+        views.BookListView.as_view(),
+        name="book_list"),
+    path(
+        'book/<int:pk>/',
+        views.BookDetailView.as_view(),
+        name="book_detail"),
+    path(
+        'book/add/',
+        views.BookCreateView.as_view(),
+        name="book_add"),
+    path(
+        'book/<int:pk>/edit/',
+        views.BookUpdateView.as_view(),
+        name="book_edit"),
+    path(
+        'book/<int:pk>/borrow/',
+        views.BookBorrowView.as_view(),
+        name="book_borrow"),
 ]
