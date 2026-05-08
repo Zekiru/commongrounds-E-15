e534242f413e29653d91dfefebe41af3fc0db826
Zeke_L5 <zekiel.villasurda@gmail.com>
2026-05-08 15:45:22 +0800

    Revert Test Fix

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -45,7 +45,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 TAILWIND_APP_NAME = 'theme'
 
 INSTALLED_APPS = [
-    # 'cloudinary_storage',  # debug purposes
+    'cloudinary_storage',  # debug purposes
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
