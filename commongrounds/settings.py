57ede048b6b01edaeae5d1834ad5038ba5729541
Zeke_L5 <zekiel.villasurda@gmail.com>
2026-05-08 15:53:06 +0800

    Another Possible Fix

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -136,9 +136,8 @@ AUTH_PASSWORD_VALIDATORS = [
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'theme', 'static'),
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if DEBUG:
     # Local Development
