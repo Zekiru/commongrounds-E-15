6b52810e63a85557e5b77bb84280752279ad88ad
Zeke_L5 <zekiel.villasurda@gmail.com>
2026-05-08 15:01:23 +0800

    Update STATICFILES_DIRS

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -136,8 +136,8 @@ AUTH_PASSWORD_VALIDATORS = [
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'theme', 'static'),
+    BASE_DIR / 'static',
+    BASE_DIR / 'theme' / 'static',
 ]
 
 if DEBUG:
